2807fc6b2de4e2e5f83cbf07d97019ff2c25fa16
Nahla-salah <ns7114346@gmail.com>
2026-03-14 23:14:00 +0200

    fix: remove base path and add js headers

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // 1. Add this import
 
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss(), // 2. Add this to the plugins array
   ],
-  base: '/', 
 })
+
